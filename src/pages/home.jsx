5bab095c5c17c7558c74afdda27bb291d69fdbec
Nico Bayer <82388053+NicoB-Code@users.noreply.github.com>
2023-01-09 22:59:24 +0100

    Dev/sprint1 basic structure merge into develop  (#5)
    🧱 added sprint 1 basic structure
    
    Co-authored-by: Thibse <thibault.rey@outlook.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Page,
+  Searchbar,
   Navbar,
   NavTitle,
   NavTitleLarge,
@@ -9,23 +10,89 @@ import {
   Block,
 } from 'framework7-react';
 
-const HomePage = () => (
-  <Page name="home">
-    {/* Top Navbar */}
-    <Navbar large>
-      <NavTitle>Lost</NavTitle>
-      <NavTitleLarge>Lost</NavTitleLarge>
-    </Navbar>
-    {/* Toolbar */}
-    <Toolbar bottom>
-      <Link>Left Link</Link>
-      <Link>Right Link</Link>
-    </Toolbar>
-    {/* Page content */}
-    <Block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </Block>
+// import leaflet stuff
+// import nominatim stuff
+// import wikipedia stuff
+// import Routenberechnungs stuff
 
-  </Page>
-);
-export default HomePage;
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curLoc: {
+        lat: 0,
+        lng: 0,
+        accuracy: 0
+      }
+    }
+  }
+  componentDidMount(){
+    // get current Location
+    // set interval to update current Location
+  }
+
+  /**
+   * This function searches for a Location by text or its OSM ID and returns the location
+   * @param {string} pSearch - This is the text which is put into the search field
+   * @param {string | undefined} osmID - OSM ID is a numerical identifier that is assigned to every element in the OpenStreetMap (OSM) database. 
+   * (https://web.locationiq.com/glossary/osm-id)
+   */
+  searchLocation = async(pSearch, osmID = undefined) => {
+    // if the OSM ID is undefined, use text search
+  }
+
+  /**
+   * This function  calls the nominatim API with a text search and returns a place
+   * @param {*} pSearch - This is the text which is put into the search field
+   */
+  getLocByText = async pSearch => {
+  }
+
+  /**
+   * This function calls the nominatim API with a text search and returns a place
+   * @param {*} pCoordinates - These are the Coordiantes that have been put into the search field
+   */
+  getLocByCoords = async (pCoordinates) => {
+  }
+
+  /**
+   * This function calls the nominatim API with a text search and returns a place
+   * @param {*} pOsmID - OSM ID is a numerical identifier that is assigned to every element in the OpenStreetMap (OSM) database. 
+   */
+  getLocByOsmID = async pOsmID => {
+  }
+
+  render() {
+    return(
+      <Page name="home">
+      {/* Top Navbar */}
+
+      <Navbar large>
+        <NavTitle>Lost</NavTitle>
+      </Navbar>
+      {/* Toolbar */}
+      <Toolbar bottom>
+        <Link>Left Link</Link>
+        <Link>Right Link</Link>
+      </Toolbar>
+      {/* Page content */}
+      {/* Map Container */}
+      {/*Wikipedia Info*/}
+      {/*Adresse und andere Info */}
+      <Searchbar
+            style={{ height: 70, margin: 0 }}
+            value={this.state.searchText}
+            placeholder="Search for a place, address, or coordinates (in Format latitude, longitude please)"
+            onChange={event => {
+              this.setState({ searchText: event.target.value, });
+            }}
+          />
+      <Block strong>
+        <p>This is the base structure for the lost project. </p>
+      </Block>
+
+    </Page>
+    )
+  }
+}
+export default Home;
