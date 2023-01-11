8488b7201375ae10d7e629f2bce3ef803a58449b
LukasAdrion <Lukas@Adrion.com>
2023-01-11 15:17:46 +0100

    :sparkles: add the reverse geocoding functions

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -36,24 +36,58 @@ class Home extends React.Component {
   }
 
   /**
-   * This function  calls the nominatim API with a text search and returns a place
-   * @param {*} pSearch - This is the text which is put into the search field
+   * This function generates a URL with the given coordinate.
+   * This URL will be used to call the nominatim API.
+   * @param {Float} lat - The latitude of the coordinate
+   * @param {Float} lon - The longitude of the coordinate
    */
-  getLocByText = async pSearch => {
+  makeUrl = async (lat, lon) => {
+    let url = "https://nominatim.openstreetmap.org/reverse?lat=" + lat + "&lon=" + lon + "&format=json";
+    return url;
   }
 
   /**
-   * This function calls the nominatim API with a text search and returns a place
-   * @param {*} pCoordinates - These are the Coordiantes that have been put into the search field
+   * This function calls the {@link makeUrl} Method to generate a URL and sends a HTTP GET request.
+   * @param {*} callback - The callback of the HTTP GET request
+   * @param {Float} lat - The latitude of the coordinate
+   * @param {Float} lon - The longitude of the coordinate
    */
-  getLocByCoords = async (pCoordinates) => {
-  }
+  getJSON = function(callback, lat, lon) {
+    let url = makeUrl(lat, lon);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+  };
 
   /**
-   * This function calls the nominatim API with a text search and returns a place
-   * @param {*} pOsmID - OSM ID is a numerical identifier that is assigned to every element in the OpenStreetMap (OSM) database. 
+   * This function calls the {@link getJSON} Method .It returns the @param location object, 
+   * if the request was successfull. Else the @param err (error) will be returned.
+   * @param {Float} lat - The latitude of the coordinate
+   * @param {Float} lon - The longitude of the coordinate
+   * 
+   * @return {JSON} The location json object of the nearest location.
    */
-  getLocByOsmID = async pOsmID => {
+  getLocationInfo = function(lat, lon){
+    getJSON(
+      function(err, location) {
+        if (err !== null) {
+          alert('Something went wrong: ' + err);
+          return err;
+        } else {
+          console.log(location);
+          return location;
+        }
+      }, lat, lon);
   }
 
 
