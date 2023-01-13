aa1f026b43561af62564f3ddb1736e8606aa69ab
Thibault Rey <66947803+Thibse@users.noreply.github.com>
2023-01-14 00:21:59 +0100

    :sparkles: implement Dev/sprint1 leaflet (#8)
    * :sparkles: add leaflet map
    
    - add leaflet map
    - add open streets map as tilelayer
    - add basic navigationbar
    
    * :coffin: remove comments, unused variables
    
    * :sparkles: add location marker
    
    - (re)places marker on map click event
    - binds coordinates to parent component
    
    * :bulb: update LocationMarker doc
    
    - add props + return doc
    
    * :sparkles: add user position marker
    
    - add position point
    - add accuracy circle around
    (triggers on map click even event)
    
    * :lipstick: change leaflet control to f7-theme
    
    * :sparkles: add 'locate user' button
    
    - click to show your current position
    - click again to stop active locating
        -> keeps last position
    
    * :lipstick: add custom location marker
    
    * :recycle: change center of map to berlin
    
    * :lipstick: active click on zoom controls
    
    - add color effect on press
    
    * :lipstick: change location marker size
    
    * :bug: accuracy circle user position
    
    - add correct representation of position accuracy
    
    * :coffin: null check
    
    - remove check for null -> never null at these points
    
    * :memo: update jsdoc
    
    Co-authored-by: NicoB-Code <bayernico@web.de>
    
    * :art: update formatting in map.scss
    
    Co-authored-by: NicoB-Code <bayernico@web.de>

---
+++ src/components/map.jsx
@@ -0,0 +1,92 @@
+import React, { createRef } from "react";
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
+import LocationMarker from "./map-assets/LocationMarker";
+import UserLocationMarker from "./map-assets/UserLocationMarker";
+
+import '../css/map.scss';
+import { Button, f7 } from "framework7-react";
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.map = createRef();
+    this.locationCoordinates = createRef();
+    this.userPos = null;
+    this.state = {
+      currentPos: {
+        latlng: null,
+        accuracy: 0
+      },
+      isLocating: false
+    }
+  }
+
+  /**
+   * This function auto-detects changes to the map size and updates the map correspondingly.
+   * Leaflet renders incorrectly when initializing in the background, thus this function is needed.
+   */
+  rerenderMap() {
+    let wait = setInterval(() => {
+      if (this.map.current) {
+        this.map.current.invalidateSize();
+        clearInterval(wait);
+      }
+    }, 10);
+  }
+
+  /**
+   * This function is executed, when the PWA is started.
+   * It first requests the user's permission to access their location on their device.
+   * If permission is granted, the users current position is used and saved in a state variable.
+   * For requesting the current user locations 'https' is required.
+   */
+  startLocalization() {
+    this.setState({ isLocating: true });
+    this.map.current.locate({
+      watch: true,
+      setView: true,
+      enableHighAccuracy: true
+    });
+    this.map.current.addEventListener('locationfound', (e) => {
+      this.state.currentPos = e.latlng;
+      this.setState({ currentPos: { latlng: e.latlng, accuracy: e.accuracy } });
+    });
+    this.map.current.addEventListener('locationerror', (e) => {
+      if (e.code == 1) {
+        this.stopLocalization();
+        f7.dialog.alert('App need\'s access to you\'re current location.');
+      }
+    })
+  }
+
+  /**
+   * This function stops the tracking of the current location of the user
+   */
+  stopLocalization() {
+    this.map.current.stopLocate();
+    this.setState({ isLocating: false });
+  }
+
+
+  render() {
+    return (
+      <>
+        <MapContainer ref={this.map} className="map" center={[52.520008, 13.404954]} zoom={10}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <LocationMarker setCoordinates={pos => this.locationCoordinates = pos} />
+          <UserLocationMarker position={this.state.currentPos} />
+
+        </MapContainer>
+        <Button onClick={() => this.state.isLocating ? this.stopLocalization() : this.startLocalization()} className={`userPosBtn ${this.state.isLocating ? 'locating' : null}`} fill
+          iconIos={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} iconF7={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} iconMd={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} />
+      </>
+    )
+  }
+}
+
+export default Map;
