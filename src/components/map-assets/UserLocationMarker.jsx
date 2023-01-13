d3f7a12f8f25cf215a2e93c29a33a899d95b1926
Thibse <thibault.rey@outlook.com>
2023-01-13 21:55:40 +0100

    :sparkles: add 'locate user' button
    - click to show your current position
    - click again to stop active locating
        -> keeps last position

--- src/components/map-assets/UserLocationMarker.jsx
+++ src/components/map-assets/UserLocationMarker.jsx
@@ -1,39 +1,17 @@
 import React from "react";
-import { CircleMarker, useMapEvents } from "react-leaflet";
+import { Button, f7 } from "framework7-react";
+import { CircleMarker, useMap, useMapEvents } from "react-leaflet";
 
 /**
- * Check for Users Location and add's Location Marker
- * @param {*} props setCoordinates in parent component
+ * Add's Location Marker on given position parameter
+ * @param {*} props {latlng, accuracy} of position
  * @returns Location Marker for User Position with Accuracy Circle
  */
 function UserLocationMarker(props) {
-  const [position, setPosition] = React.useState(null);
-  const [accuracy, setAccuracy] = React.useState(null);
-  let firstLocalization = false;
-
-  const map = useMapEvents({
-    click() {
-      firstLocalization = true;
-      map.locate({
-        watch: true,
-        enableHighAccuracy: true
-      });
-    },
-    locationfound(e) {
-      if (firstLocalization) {
-        map.setView(e.latlng, 10);
-        firstLocalization = false;
-      }
-      setPosition(e.latlng);
-      props.setCoordinates(e.latlng);
-      setAccuracy(e.accuracy);
-    }
-  })
-
-  return position === null ? null : (
+  return props.position.latlng === null ? null : (
     <>
-      <CircleMarker center={position ?? null} radius={accuracy} />
-      <CircleMarker center={position ?? null} fillOpacity={1} />
+      <CircleMarker center={props.position.latlng ?? null} radius={props.position.accuracy / 2} />
+      <CircleMarker center={props.position.latlng ?? null} fillOpacity={1} />
     </>
   )
 }
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,42 +1,89 @@
 import React, { createRef } from "react";
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 import LocationMarker from "./map-assets/LocationMarker";
 import UserLocationMarker from "./map-assets/UserLocationMarker";
 
 import '../css/map.scss';
+import { Button, f7 } from "framework7-react";
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.map = createRef();
     this.locationCoordinates = createRef();
-    this.userCoordinates = createRef();
+    this.userPos = null;
+    this.state = {
+      currentPos: {
+        latlng: null,
+        accuracy: 0
+      },
+      isLocating: false
+    }
   }
-  
+
   /**
    * wait till map ist ready an trigger resize
    * -> leaflet renders incorrectly when initializing in the background
    */
   rerenderMap() {
     let wait = setInterval(() => {
-      if(this.map.current) {
+      if (this.map.current) {
         this.map.current.invalidateSize();
         clearInterval(wait);
       }
     }, 10);
   }
 
+  /**
+   * start locating user position
+   * saves position data in state variable
+   * request's for location access on device -> cannot access, if connected via http
+   */
+  startLocalization() {
+    this.setState({ isLocating: true });
+    this.map.current.locate({
+      watch: true,
+      setView: true,
+      enableHighAccuracy: true
+    });
+    this.map.current.addEventListener('locationfound', (e) => {
+      this.state.currentPos = e.latlng;
+      this.setState({ currentPos: { latlng: e.latlng, accuracy: e.accuracy } });
+    });
+    this.map.current.addEventListener('locationerror', (e) => {
+      if (e.code == 1) {
+        this.stopLocalization();
+        f7.dialog.alert('App need\'s access to you\'re current location.');
+      }
+    })
+  }
+
+  /**
+   * stops locating user position
+   */
+  stopLocalization() {
+    this.map.current.stopLocate();
+    this.setState({ isLocating: false });
+  }
+
+
   render() {
     return (
-      <MapContainer ref={this.map} className="map" center={[51.505, -0.09]} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LocationMarker setCoordinates={pos => this.locationCoordinates = pos} />
-        <UserLocationMarker setCoordinates={pos => this.userCoordinates = pos} />
-      </MapContainer>
+      <>
+        <MapContainer ref={this.map} className="map" center={[51.505, -0.09]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <LocationMarker setCoordinates={pos => this.locationCoordinates = pos} />
+          <UserLocationMarker position={this.state.currentPos} />
+
+        </MapContainer>
+        <Button onClick={() => this.state.isLocating ? this.stopLocalization() : this.startLocalization()} className={`userPosBtn ${this.state.isLocating ? 'locating' : null}`} fill
+          iconIos={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} iconF7={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} iconMd={this.state.isLocating ? 'f7:location_slash' : 'f7:location'} />
+      </>
     )
   }
 }
