834b17e07c30011d843fd1640dff98ff678c6e22
Thibse <thibault.rey@outlook.com>
2023-01-15 18:21:29 +0100

    :art: update formatting

--- src/js/wikiAPI.js
+++ src/js/wikiAPI.js
@@ -10,29 +10,29 @@
  * @returns A Promise which contains the API request.
  */
 function wikiSearch(lat, lon, city) {
-    return new Promise((resolve, reject) => {
-        let article = {
-            title: "",
-            text: ""
-        };
-        getGeoSearch(lat, lon, res_geo => {
-            if ((res_geo.query.geosearch).length !== 1) {
-                getCity(city, res_city => {
-                    article.title = res_city.query.pages[Object.keys(res_city.query.pages)[0]].title;
-                    getWikiText(article.title, res_text => {
-                        article.text = res_text.query.pages[Object.keys(res_text.query.pages)[0]].extract;
-                        resolve(article);
-                    });
-                });
-            } else {
-                article.title = res_geo.query.geosearch[0].title;
-                getWikiText(article.title, res_text => {
-                    article.text = res_text.query.pages[Object.keys(res_text.query.pages)[0]].extract;
-                    resolve(article);
-                });
-            }
+  return new Promise((resolve, reject) => {
+    let article = {
+      title: "",
+      text: ""
+    };
+    getGeoSearch(lat, lon, res_geo => {
+      if ((res_geo.query.geosearch).length !== 1) {
+        getCity(city, res_city => {
+          article.title = res_city.query.pages[Object.keys(res_city.query.pages)[0]].title;
+          getWikiText(article.title, res_text => {
+            article.text = res_text.query.pages[Object.keys(res_text.query.pages)[0]].extract;
+            resolve(article);
+          });
         });
+      } else {
+        article.title = res_geo.query.geosearch[0].title;
+        getWikiText(article.title, res_text => {
+          article.text = res_text.query.pages[Object.keys(res_text.query.pages)[0]].extract;
+          resolve(article);
+        });
+      }
     });
+  });
 }
 
 /**
@@ -44,8 +44,8 @@ function wikiSearch(lat, lon, city) {
  * @param {function} callback - The action which gets called as soon as the response is ready
  */
 function getGeoSearch(lat, lon, callback) {
-    runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&list=geosearch&gsradius=75&gscoord=" + lat + "|" + lon + "&format=json&gslimit=1").then((res) => {callback(res)}
-        , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
+  runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&list=geosearch&gsradius=75&gscoord=" + lat + "|" + lon + "&format=json&gslimit=1").then((res) => { callback(res) }
+    , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
 }
 
 /**
@@ -56,8 +56,8 @@ function getGeoSearch(lat, lon, callback) {
  * @param {function} callback - The action which gets called as soon as the response is ready
  */
 function getWikiText(datag, callback) {
-    runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&exsentences=5&titles=" + datag + "&explaintext=true&exsectionformat=plain&format=json").then((res) => {callback(res)}
-        , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
+  runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&exsentences=5&titles=" + datag + "&explaintext=true&exsectionformat=plain&format=json").then((res) => { callback(res) }
+    , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
 }
 
 /**
@@ -68,8 +68,8 @@ function getWikiText(datag, callback) {
  * @param {function} callback - The action which gets called as soon as the reponse is ready
  */
 function getCity(cityg, callback) {
-    runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrlimit=1&gsrsearch='" + cityg + "'").then((res) => {callback(res)}
-        , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
+  runAPI("https://de.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrlimit=1&gsrsearch='" + cityg + "'").then((res) => { callback(res) }
+    , rejection => console.log("Wikipedia rejected the API-Request with the error: " + rejection));
 }
 
 /**
@@ -79,20 +79,20 @@ function getCity(cityg, callback) {
  * @returns - A promise which contains the executed API request
  */
 function runAPI(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                resolve(this.response);
-            } else {
-                reject(xhr.status);
-            }
-        };
-        xhr.onerror = reject;
-        xhr.send();
-    });
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(xhr.status);
+      }
+    };
+    xhr.onerror = reject;
+    xhr.send();
+  });
 }
 
 export default wikiSearch;
