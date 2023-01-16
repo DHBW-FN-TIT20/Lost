9931f56a3daed05f1d53f0900c3f11e0fc4a5282
Thibse <thibault.rey@outlook.com>
2023-01-16 12:13:03 +0100

    :recycle: change to react component class

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,34 +1,37 @@
-import { Icon } from "framework7-react";
-import React, { useEffect } from "react";
+import React from "react";
 import Draggable from 'react-draggable';
 
 import '../css/modal.scss';
 
-function SheetModal() {
-  const draggable = React.useRef(null);
+class SheetModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.draggable = React.createRef(null);
+  }
 
-  React.useEffect(() => {
-    window.addEventListener('resize', positioning);
-  })
+  componentDidMount() {
+    window.addEventListener('resize', this.positioning);
+  }
 
-  const positioning = () => {
-    if (draggable.current.state.y < window.innerHeight * 0.4) {
-      draggable.current.setState({y: window.innerHeight * 0.05})
+  positioning() {
+    if (this.draggable.current.state.y < window.innerHeight * 0.4) {
+      this.draggable.current.setState({y: window.innerHeight * 0.05})
     } 
-    else if (draggable.current.state.y < window.innerHeight * 0.7) 
+    else if (this.draggable.current.state.y < window.innerHeight * 0.7) 
     {
-      draggable.current.setState({y: window.innerHeight * 0.6});
+      this.draggable.current.setState({y: window.innerHeight * 0.6});
     }
-    else if (draggable.current.state.y < window.innerHeight * 0.9) {
-      draggable.current.setState({y: window.innerHeight * 0.8});
+    else if (this.draggable.current.state.y < window.innerHeight * 0.9) {
+      this.draggable.current.setState({y: window.innerHeight * 0.8});
     }
     else {
-      draggable.current.setState({y: window.innerHeight-56-20})
+      this.draggable.current.setState({y: window.innerHeight-56-20})
     }
   }
 
-  return (
-    <Draggable ref={draggable} defaultPosition={{x: 0, y:window.innerHeight-56-20}} axis='y' bounds={{ left: 0, top: window.innerHeight*0.05, right: 0, bottom: 1200 }} onStop={() => positioning()} >
+  render() {
+    return (
+      <Draggable ref={this.draggable} defaultPosition={{x: 0, y:window.innerHeight-56-20}} axis='y' bounds={{ left: 0, top: window.innerHeight*0.05, right: 0, bottom: 1200 }} onStop={() => this.positioning()} >
       <div className="box">
         <a className="slider" />
         <div className="modal-content">
@@ -36,7 +39,8 @@ function SheetModal() {
         </div>
       </div>
     </Draggable>
-  )
+    )
+  }
 }
 
 export default SheetModal;
