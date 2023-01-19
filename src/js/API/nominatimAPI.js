2b8987e85199b6df237648f4ec0847bb89219d91
Thibse <thibault.rey@outlook.com>
2023-01-19 15:14:37 +0100

    :sparkles: add Dev/sprint1 nominatim (#11)
    commit 8bbb854216241a4ab2c91e269dc706f7732b6da5
    Author: LukasAdrion <Lukas@Adrion.com>
    Date:   Sun Jan 15 20:03:49 2023 +0100
    
        :truck: create a folder for the API calls
        - outsourced the http get request
    
    commit 16d60f55466b67cf125353ec15599e2bad9b850e
    Author: LukasAdrion <Lukas@Adrion.com>
    Date:   Sun Jan 15 16:46:42 2023 +0100
    
        :construction: integrate promise
    
    commit a6a5d0d6051d03f85bac81cd4b5d06343da2e9a9
    Author: LukasAdrion <Lukas@Adrion.com>
    Date:   Sun Jan 15 16:02:03 2023 +0100
    
        :truck: move nominatim to extra file
    
    commit 8c55f37b857be7e6c11954c55faada9d9d052688
    Author: LukasAdrion <Lukas@Adrion.com>
    Date:   Wed Jan 11 19:05:04 2023 +0100
    
        :pencil2: alter the function syntax
    
    commit 8488b7201375ae10d7e629f2bce3ef803a58449b
    Author: LukasAdrion <Lukas@Adrion.com>
    Date:   Wed Jan 11 15:17:46 2023 +0100
    
        :sparkles: add the reverse geocoding functions

---
+++ src/js/API/nominatimAPI.js
@@ -0,0 +1,54 @@
+//getLocationInfo(48.401081,9.987608).then((props) => {console.log(props)})
+
+import httpGet from "./httpGetAPI";
+
+/**
+ * This function generates a URL with the given coordinate.
+ * This URL will be used to call the nominatim API.
+ * @param {Float} lat - The latitude of the coordinate
+ * @param {Float} lon - The longitude of the coordinate
+ */
+function makeUrl(lat, lon) {
+  let url =
+    "https://nominatim.openstreetmap.org/reverse?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&format=json";
+  return url;
+}
+
+/**
+ * This function calls the {@link makeUrl} Method to generate a URL and sends a HTTP GET request.
+ * @param {*} callback - The callback of the HTTP GET request
+ * @param {Float} lat - The latitude of the coordinate
+ * @param {Float} lon - The longitude of the coordinate
+ */
+function getJSON(callback, lat, lon) {
+  let url = makeUrl(lat, lon);
+  httpGet(url).then((res) => {
+    callback(res);
+  }, 
+  rejection => console.log("nominatim rejected the API-request with error: "+ rejection));
+}
+
+/**
+ * This function calls the {@link getJSON} Method. It returns the @param location object,
+ * if the request was successfull. Else the @param err (error) will be returned.
+ * @param {Float} lat - The latitude of the coordinate
+ * @param {Float} lon - The longitude of the coordinate
+ *
+ * @return {JSON} The location json object of the nearest location.
+ */
+function getLocationInfo(lat, lon) {
+  return new Promise((resolve, reject) => {
+    getJSON(
+      function (location) {
+        resolve(location);
+      },
+      lat,lon
+    );
+  });
+}
+
+export default getLocationInfo;
