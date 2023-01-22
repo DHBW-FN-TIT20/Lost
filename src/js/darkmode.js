a3fd426bb52888304692fca5c3f6b69fd813eb8f
Ehlikopter <ralfehli@gmail.com>
2023-01-22 21:39:23 +0100

    📝correct jsdoc

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -2,8 +2,7 @@
   import {f7} from "framework7-react";
   
   /**
-   * This function gets called if the dark mode toggle changes.
-   * It changes the current dark/white mode depending on the {@link evt} parameter.
+   * This function changes the current dark/white mode depending on the {@link evt} parameter.
    * @param {boolean} evt - event state
    */
   function changeDarkmode(evt){
--- src/pages/favorite.jsx
+++ src/pages/favorite.jsx
@@ -50,7 +50,7 @@ class Favorite extends React.Component {
 
   /**
    * This function gets called if the dark mode toggle changes.
-   * It changes the current dark/white mode depending on the {@link evt} parameter.
+   * sets the state and calls changeDarkmode
    * @param {boolean} evt - event state
    */
   toggleDarkmode = (evt) => {
