a1b08205db962f06ed0bf9a1ca851ecbfe007ecc
Nico Bayer <82388053+NicoB-Code@users.noreply.github.com>
2023-01-22 11:26:34 +0100

    :memo: applied suggested changes
    Due to @LukasAdrion allowing me to do so

--- src/js/API/httpGetAPI.js
+++ src/js/API/httpGetAPI.js
@@ -1,8 +1,12 @@
 
 /**
  * This function creates a http get request to the {@param url}
+ * The request gets sent via. XMLHttpRequest (xhr.send()).
+ * If the request is asynchronous, this method returns as soon as the request is sent.
+ * The promise is resolved / rejected by the XHR API.
  * @param {String} url - the url for the get request
- * @returns 
+ * @returns - A promise, which contains the answer to the http request. 
+ *            If XHR-Api returns status 200 (success) the promise is resolved, otherwise it will be rejected.
  */
 function httpGet(url) {
   return new Promise((resolve, reject) => {
